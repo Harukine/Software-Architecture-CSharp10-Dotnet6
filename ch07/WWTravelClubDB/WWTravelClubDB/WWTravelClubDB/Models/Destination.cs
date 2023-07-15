4f11ea4b71b03ca6ce59f732d5635e1ae32d1c14
Jacob Nguyen <jacobhtnguyen@gmail.com>
2023-07-15 23:30:59 +0900

    added models folder and destination model

---
+++ ch07/WWTravelClubDB/WWTravelClubDB/WWTravelClubDB/Models/Destination.cs
@@ -0,0 +1,10 @@
+namespace WWTravelClubDB.Models
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string? Description { get; set; }
+    }
+}
