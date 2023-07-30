06d39ed1d6b07d17dc23eb7873814d59df824444
Jacob Nguyen <jacobhtnguyen@gmail.com>
2023-07-31 00:08:20 +0900

    added test to update db entry

--- ch07/WWTravelClubDB/WWTravelClubDB/WWTravelClubDBTest/Program.cs
+++ ch07/WWTravelClubDB/WWTravelClubDB/WWTravelClubDBTest/Program.cs
@@ -8,33 +8,55 @@ Console.ReadKey();
 var context = new LibraryDesignTimeDbContextFactory()
     .CreateDbContext();
 
-var firstDestination = new Destination
-{
-    Name = "Florence",
-    Country = "Italy",
-    Packages = new List<Package>()
-    {
-        new Package
-        {
-            Name = "Summer in Florence",
-            StartValidityDate = new DateTime(2019, 6, 1),
-            EndValidityDate = new DateTime(2019, 10, 1),
-            DurationInDays=7,
-            Price=1000
-        },
-        new Package
-        {
-            Name = "Winter in Florence",
-            StartValidityDate = new DateTime(2019, 12, 1),
-            EndValidityDate = new DateTime(2020, 2, 1),
-            DurationInDays=7,
-            Price=500
-        }
-    }
-};
+// First database entry, inserting data
+// var firstDestination = new Destination
+// {
+//     Name = "Florence",
+//     Country = "Italy",
+//     Packages = new List<Package>()
+//     {
+//         new Package
+//         {
+//             Name = "Summer in Florence",
+//             StartValidityDate = new DateTime(2019, 6, 1),
+//             EndValidityDate = new DateTime(2019, 10, 1),
+//             DurationInDays=7,
+//             Price=1000
+//         },
+//         new Package
+//         {
+//             Name = "Winter in Florence",
+//             StartValidityDate = new DateTime(2019, 12, 1),
+//             EndValidityDate = new DateTime(2020, 2, 1),
+//             DurationInDays=7,
+//             Price=500
+//         }
+//     }
+// };
 
-context.Destinations.Add(firstDestination);
+// context.Destinations.Add(firstDestination);
+// await context.SaveChangesAsync();
+
+// Console.WriteLine($"DB populated: first destination id is {firstDestination.Id}");
+// Console.ReadKey();
+
+
+// Second database entry, modify data
+var toModify = await context.Destinations
+    .Where(m => m.Name == "Florence")
+    .Include(m => m.Packages)
+    .FirstOrDefaultAsync();
+
+toModify.Description = "Florence is a famous historical Italian town";
+
+foreach (var package in toModify.Packages)
+    package.Price = package.Price * 1.1m;
+
 await context.SaveChangesAsync();
 
-Console.WriteLine($"DB populated: first destination id is {firstDestination.Id}");
+var verifyChanges = await context.Destinations
+    .Where(m => m.Name == "Florence")
+    .FirstOrDefaultAsync();
+
+Console.WriteLine($"New Florence description: {verifyChanges.Description}");
 Console.ReadKey();
